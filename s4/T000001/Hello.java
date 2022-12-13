2bef0c6fb815c13a00604d0dc6417c06d0afec8c
tut223364 <120451598+tut223364@users.noreply.github.com>
2022-12-13 17:25:54 +0900

    Update Hello.java

--- s4/T000001/Hello.java
+++ s4/T000001/Hello.java
@@ -3,7 +3,7 @@ import java.lang.*;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("GitHub(T000001)");
+        System.out.println("(T000001)");
     }
 }
 
